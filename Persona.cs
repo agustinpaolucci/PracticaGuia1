a3ae2112155c39d9971b3de149b2cdc422b67a15
agustinpaolucci <globalprint@gmail.com>
2022-11-30 13:16:25 -0300

    Agregar archivos de proyecto.

---
+++ Persona.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaGuia1
+{
+    public class Persona
+    {
+        // ATRIBUTOS
+        private string nombre; // Campo de cada objeto Persona que almacena su nombre
+
+        private int edad; // Campo de cada objeto Persona que almacena su edad
+
+        private string dni; // Campo de cada objeto Persona que almacena su NIF
+
+
+        // PROPERTIES
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+        public string Dni
+        {
+            get { return dni; }
+            set { dni = value; }
+        }
+
+        // CONSTRUCTOR SIN PARAMETROS
+
+        public Persona()
+        {
+            nombre = string.Empty;
+            edad = 0;
+            dni = string.Empty;
+        }
+
+        // CONSTRUCTOR CON 2 PARAMETROS
+        public Persona(string nombre, string dni)
+        {
+            this.nombre = nombre;
+            this.dni = dni;
+        }
+
+        // CONSTRUCTOR CON 2 PARAMETROS
+        public Persona(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        // CONSTRUCTOR CON 3 PARAMETROS
+        public Persona(string nombre, int edad, string dni)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.dni = dni;
+        }
+
+        
+
+
+        public void Cumpleanos()
+        {
+            edad++;
+        }
+
+
+
+
+
+
+
+
+        // METODO TOSTRING
+        public override string ToString()
+        {
+            return "\nNOMBRE: " + nombre + "\nEDAD: " + edad + "\nDNI: " + dni;
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaGuia1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p01 = new Persona("Agustin",47,"24.603.259");
+            Persona p02 = new Persona("Maria", 39);
+            Persona p03 = new Persona("Pedro", 22, "40.555.775") ;
+            Persona p04 = new Persona("Juana","16.620.902");
+            Persona p05 = new Persona();
+
+            p03.Edad = 25;
+            p01.Nombre = "Guillermo";
+
+            p01.Cumpleanos();
+
+            Console.WriteLine(p01.ToString());
+            Console.WriteLine(p02.ToString());
+            Console.WriteLine(p03.ToString());
+            Console.WriteLine(p04.ToString());
+            Console.WriteLine(p05.ToString());
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Presione una tecla para finalizar...");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+/*
+
+<tipoDevuelto> <nombreMétodo> (<parametros>) 
+{
+    <instrucciones>
+}
+*/
